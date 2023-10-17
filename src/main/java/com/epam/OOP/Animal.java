fc4177c77697b22341998a6fb301223fe3b070bf
Kvasniuk Denis <denis.kvasniuk23@gmail.com>
2023-10-17 14:50:55 +0300

    Solved

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -4,6 +4,7 @@ public class Animal {
     private String color;
     private int numberOfPaws;
     private boolean hasFur;
+
     public Animal(String color, int numberOfPaws, boolean hasFur){
         this.color = color;
         this.numberOfPaws = numberOfPaws;
