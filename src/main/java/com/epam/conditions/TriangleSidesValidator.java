3cde1cf5b12a1175e6ad134821a8414d9a215788
Kvasniuk Denis <denis.kvasniuk23@gmail.com>
2023-10-18 15:15:15 +0300

    Solved

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,14 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
+            if(firstSide + secondSide < thirdSide  || secondSide + thirdSide < firstSide || firstSide + thirdSide < secondSide)
+            {
+                System.out.println("it's not a triangle");
 
+            }
+            else {
+                System.out.println("this is a valid triangle");
+            }
     }
 
 }
