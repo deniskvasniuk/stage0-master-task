067fb0ef7819bdfb398e6d8682c606e2b0b48749
Kvasniuk Denis <denis.kvasniuk23@gmail.com>
2023-10-21 20:14:46 +0300

    Solved

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,23 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
+        StringBuilder result = new StringBuilder();
 
+        for (int i = 1; i <= cathetusLength; i++) {
+
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                result.append(" ");
+            }
+
+
+            for (int j = 1; j <= i; j++) {
+                result.append("*");
+            }
+
+            result.append("\n");
+        }
+
+        System.out.print(result.toString());
     }
 
     public static void main(String[] args) {
