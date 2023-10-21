ee32d63e6a4efcefa9f89ec1a672637ce58a6a4b
Kvasniuk Denis <denis.kvasniuk23@gmail.com>
2023-10-21 19:48:26 +0300

    Solved

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -4,6 +4,7 @@ public class DaysInMonth {
 
     public void printDays(int year, int month) {
 
+
     }
 
 }
--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -1,6 +1,7 @@
 package com.epam.conditions;
 
-public class TriangleSidesValidator {
+public class
+TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
             if(firstSide + secondSide < thirdSide  || secondSide + thirdSide < firstSide || firstSide + thirdSide < secondSide)
